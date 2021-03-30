a465e5424c6789275cff7bbf45d61e9d4533bbc4
taras1hupalyk <t.hupalyk@gmail.com>
2021-03-30 05:16:16 +0300

    Added Teacher Conteroller

---
+++ GradeBook.Api/Services/TeacherService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GradeBook.Data.Repositories;
+using GradeBook.Data.Repositories.Intefaces;
+
+namespace GradeBook.Api.Services
+{
+    public class TeacherService
+    {
+        public TeacherService(ITeacherRepository teacherRepository)
+        {
+        }
+    }
+}
